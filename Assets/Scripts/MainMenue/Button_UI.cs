cea9cc67319b89873acd228fb0f52a745ed3dfef
SkyWalker-2001 <126860277+SkyWalker-2001@users.noreply.github.com>
2024-05-20 11:17:46 +0530

    Initial commit

---
+++ Assets/Scripts/MainMenue/Button_UI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_UI : MonoBehaviour
+{
+    public void OnClick_PlayButton()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void OnClick_AMXButton()
+    {
+        Application.OpenURL("https://l.instagram.com/?u=https%3A%2F%2Flinktr.ee%2Famxteam&e=AT2eO4mFy63h6t7F206oKOkWJ_QgI 5DydwYf3YczNDGDbMczVGV73vw6Q9vyMFks0uwaLTE8L9mBFf1yY5o42qplHMDff_g0RX-knvQ");
+        Debug.Log("AMX Button is Pressed");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AMX-Sprint
+ Mobile 2D game similar to Google's dinosaur game when the connection doesn't go.
